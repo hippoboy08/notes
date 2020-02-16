2f3992d8236f1a09393cb0fdd7d8bbb757058277
Khoi Huynh <khoi.ht0181@gmail.com>
2020-02-16 15:56:52 +0200

    Remove Materialize CSS package
    Use CDN instead

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'auth', pathMatch: 'full'},
+  // {path: '', redirectTo: 'auth', pathMatch: 'full'},
+  {path: '', redirectTo: 'notes', pathMatch: 'full'},
 ];
 
 @NgModule({
